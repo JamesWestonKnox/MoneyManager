2f9fb67a4f6d94724e24328ef34433760d50e678
st10275468 <st10275468@vcconnect.edu.za>
2025-04-30 16:25:14 +0200

    Added functionality to the bottom nav bar on the pages that include it

---
+++ app/src/main/java/com/example/moneymanager/GoalsActivity.kt
@@ -0,0 +1,10 @@
+package com.example.moneymanager
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GoalsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.goals_page)
+}}
---
+++ app/src/main/java/com/example/moneymanager/TransactionsActivity.kt
@@ -0,0 +1,63 @@
+package com.example.moneymanager
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class TransactionsActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.transactions_page)
+
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNav.selectedItemId = R.id.nav_transactions
+
+
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_transactions -> {
+                    true
+                }
+
+                R.id.nav_budgets -> {
+
+                    startActivity(Intent(this, BudgetsActivity::class.java))
+                    overridePendingTransition(0, 0)
+                    finish()
+
+                    true
+                }
+                R.id.nav_home -> {
+
+                    startActivity(Intent(this, HomeActivity::class.java))
+                    overridePendingTransition(0, 0)
+                    finish()
+
+                    true
+                }
+
+                R.id.nav_reports -> {
+
+                    startActivity(Intent(this, ReportsActivity::class.java))
+                    overridePendingTransition(0, 0)
+                    finish()
+
+                    true
+                }
+
+                R.id.nav_goals -> {
+
+                    startActivity(Intent(this, GoalsActivity::class.java))
+                    overridePendingTransition(0, 0)
+                    finish()
+
+                    true
+                }
+
+
+                else -> false
+
+            }
+}}}
